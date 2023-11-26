fa2e8955b7a748a65e4868fff8407eaf8974d39c
zigota23 <maksym.volkov.zigota@gmail.com>
2023-11-26 09:30:45 +0200

    test6

--- cursach/Form1.Designer.cs
+++ cursach/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(620, 322);
+            this.button1.Location = new System.Drawing.Point(430, 376);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 35);
