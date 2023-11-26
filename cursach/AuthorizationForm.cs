d8265a7a8fbdcfc57fc1dde53f3130ffe6ba6da0
S4fara <safara4good@gmail.com>
2023-11-26 15:35:53 +0200

    add db connection sting

--- cursach/AuthorizationForm.cs
+++ cursach/AuthorizationForm.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+
 
 namespace cursach
 {
@@ -20,7 +22,18 @@ namespace cursach
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-           
+            string email = emailField.Text;
+            string password = passwordField.Text;
+        }
+
+        string StringConnection = "Server = ; port = ; user id = ; password = ; database = ;";
+        NpgsqlConnection Con;
+        NpgsqlCommand Cmd;
+        private void connection()
+        {
+            Con = new NpgsqlConnection();
+            Con.ConnectionString = StringConnection;
+            Con.Open();
         }
 
         private void SignUpButton_Click(object sender, EventArgs e)
--- cursach/RegistrationForm.Designer.cs
+++ cursach/RegistrationForm.Designer.cs
@@ -44,97 +44,108 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(464, 23);
+            this.label1.Location = new System.Drawing.Point(309, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "E-mail";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(468, 46);
+            this.textBox1.Location = new System.Drawing.Point(312, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 26);
+            this.textBox1.Size = new System.Drawing.Size(89, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(468, 107);
+            this.textBox2.Location = new System.Drawing.Point(312, 70);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(132, 26);
+            this.textBox2.Size = new System.Drawing.Size(89, 20);
             this.textBox2.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(464, 84);
+            this.label2.Location = new System.Drawing.Point(309, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "First name";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(468, 171);
+            this.textBox3.Location = new System.Drawing.Point(312, 111);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(132, 26);
+            this.textBox3.Size = new System.Drawing.Size(89, 20);
             this.textBox3.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(464, 148);
+            this.label3.Location = new System.Drawing.Point(309, 96);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.Size = new System.Drawing.Size(56, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.label3.Text = "Last name";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(468, 229);
+            this.textBox4.Location = new System.Drawing.Point(312, 149);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(132, 26);
+            this.textBox4.Size = new System.Drawing.Size(89, 20);
             this.textBox4.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(464, 206);
+            this.label4.Location = new System.Drawing.Point(309, 134);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "label4";
+            this.label4.Text = "Password";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(468, 296);
+            this.textBox5.Location = new System.Drawing.Point(312, 192);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(132, 26);
+            this.textBox5.Size = new System.Drawing.Size(89, 20);
             this.textBox5.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(464, 273);
+            this.label5.Location = new System.Drawing.Point(309, 177);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.Size = new System.Drawing.Size(90, 13);
             this.label5.TabIndex = 8;
-            this.label5.Text = "label5";
+            this.label5.Text = "Repeat password";
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(468, 368);
+            this.submitButton.Location = new System.Drawing.Point(312, 239);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(102, 35);
+            this.submitButton.Size = new System.Drawing.Size(68, 23);
             this.submitButton.TabIndex = 10;
             this.submitButton.Text = "SUBMIT";
             this.submitButton.UseVisualStyleBackColor = true;
             // 
             // RegistrationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
             this.Controls.Add(this.submitButton);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
@@ -146,6 +157,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RegistrationForm";
             this.Text = "RegistrationForm";
             this.ResumeLayout(false);
