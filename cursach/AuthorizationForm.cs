e6d540b3c68ef851b0acf8ba4c363516574cd1ff
S4fara <safara4good@gmail.com>
2023-11-27 14:41:48 +0200

    autorization function

--- cursach/AuthorizationForm.cs
+++ cursach/AuthorizationForm.cs
@@ -15,7 +15,7 @@ namespace cursach
 {
     public partial class AuthorizationForm : Form
     {
-
+        private string connectionString = "Server = localhost ; port = 5432; user id = postgres; password = root; database = cursach;";
         public AuthorizationForm()
         {
             InitializeComponent();
@@ -25,8 +25,45 @@ namespace cursach
         {
             string email = emailField.Text;
             string password = passwordField.Text;
+            AutorizationUser(email, password);
         }
+        private void AutorizationUser(string email, string password) {
 
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM users WHERE email = @email", connection))
+                {
+                    cmd.Parameters.AddWithValue("email", email);
+
+                    using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string hashedPasswordFromDB = reader["hashpass"].ToString();
+
+                            if (BCrypt.Net.BCrypt.Verify(password, hashedPasswordFromDB))
+                            {
+                                MessageBox.Show("Login successful");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Invalid password");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("User not found");
+                        }
+                    }
+                }
+
+                connection.Close();
+
+
+            }
+        }
         private void SignUpButton_Click(object sender, EventArgs e)
         {
             RegistrationForm form = new RegistrationForm();
