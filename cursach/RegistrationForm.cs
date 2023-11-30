3b9f21a43de876443cb51612bec6eaeb014e6024
Dust478 <jan11072017@gmail.com>
2023-11-30 14:45:23 +0200

    Made validation to email and password

--- cursach/RegistrationForm.cs
+++ cursach/RegistrationForm.cs
@@ -1,5 +1,6 @@
 ﻿using Npgsql;
 using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,11 +28,41 @@ namespace cursach
             string password = passwordField.Text;
             string firstname = firstNameField.Text;
             string lastname = lastNameField.Text;
-           
+            string confirmPassword = repeatPasswordField.Text;
+
             registration.RegisterUser(email, password, firstname, lastname);
-            
-            GlobalData.RegistrationForm.Hide();
-            GlobalData.AuthorizationForm.Show();
+
+            if(!IsConfirmedPassword(password, confirmPassword))
+            {
+                MessageBox.Show("Пароли не совпадают", "Проверка данных", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (IsValidEmail(email) && IsValidPassword(password))
+            {
+                GlobalData.RegistrationForm.Hide();
+                GlobalData.AuthorizationForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Введенные Email и(или) Пароль не являются действительными.", "Проверка Email и Пароля", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool IsConfirmedPassword(string password, string confirmPassword)
+        {
+            return password == confirmPassword;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            Regex regex = new Regex(emailPattern);
+            return regex.IsMatch(email);
+        }
+
+        private bool IsValidPassword(string password)
+        {
+            return password.Length >= 6;
         }
 
         protected override void OnFormClosing(FormClosingEventArgs e)
